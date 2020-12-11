af6156de42d6f1592ed260cb47637c6228b5a30b
I PUTU AGUS <I PUTU AGUS@ASUS>
2020-12-11 09:54:55 +0700

    BRANDON

---
+++ Form Pengaturan Warna/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Form_Pengaturan_Warna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.namaGanti = new System.Windows.Forms.TextBox();
+            this.buttonProses = new System.Windows.Forms.Button();
+            this.labelNama = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input Data :";
+            // 
+            // namaGanti
+            // 
+            this.namaGanti.Location = new System.Drawing.Point(62, 50);
+            this.namaGanti.Name = "namaGanti";
+            this.namaGanti.Size = new System.Drawing.Size(431, 20);
+            this.namaGanti.TabIndex = 1;
+            this.namaGanti.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // buttonProses
+            // 
+            this.buttonProses.Location = new System.Drawing.Point(62, 101);
+            this.buttonProses.Name = "buttonProses";
+            this.buttonProses.Size = new System.Drawing.Size(75, 23);
+            this.buttonProses.TabIndex = 2;
+            this.buttonProses.Text = "Proses";
+            this.buttonProses.UseVisualStyleBackColor = true;
+            this.buttonProses.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelNama
+            // 
+            this.labelNama.AutoSize = true;
+            this.labelNama.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNama.Location = new System.Drawing.Point(55, 172);
+            this.labelNama.Name = "labelNama";
+            this.labelNama.Size = new System.Drawing.Size(174, 42);
+            this.labelNama.TabIndex = 3;
+            this.labelNama.Text = "[EMPTY]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 261);
+            this.Controls.Add(this.labelNama);
+            this.Controls.Add(this.buttonProses);
+            this.Controls.Add(this.namaGanti);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form Pengaturan Warna";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox namaGanti;
+        private System.Windows.Forms.Button buttonProses;
+        private System.Windows.Forms.Label labelNama;
+    }
+}
+
---
+++ Form Pengaturan Warna/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Pengaturan_Warna
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (labelNama.Text == "[EMPTY]")
+            {
+                labelNama.Text = namaGanti.Text;
+            }
+            if (labelNama.Text != "[EMPTY]")
+            {
+                if (namaGanti.Text == "DELETE")
+                {
+                    labelNama.Text = "[EMPTY]";
+                }
+
+            }
+            if (labelNama.Text != "[EMPTY]")
+            {
+                if (namaGanti.Text == "HIDE")
+                {
+                    labelNama.Visible = false;
+
+                }
+                if (namaGanti.Text == "SHOWN")
+                {
+                    labelNama.Visible = true;
+                }
+                    
+            }
+            if (labelNama.Text != "[EMPTY]")
+            {
+                if (namaGanti.Text == "BLUE")
+                {
+                    labelNama.ForeColor = Color.Blue;
+                }
+                else if (namaGanti.Text == "RED") 
+                {
+                    labelNama.ForeColor = Color.Red;
+                }
+                else if(namaGanti.Text == "GREEN")
+                {
+                    labelNama.ForeColor = Color.Green;
+                }
+            }
+            
+        }
+    }
+}
